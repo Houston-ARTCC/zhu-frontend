3b5166ede5e82f4c4cc5c764490e8b3e0176955f
Michael Romashov <mromashov@icloud.com>
2020-12-31 00:56:46 -0500

    Added navbar dropdowns and links.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Nav, Navbar, NavbarBrand } from 'react-bootstrap'
+import { Button, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import logoColor from '../img/logo.png'
 import logoLight from '../img/logo-light.png'
 import { getFullName, parseJWT } from '../Helpers'
@@ -11,29 +11,46 @@ export default function Navigation() {
     useEffect(() => window.addEventListener('scroll', () => setScroll(window.scrollY > 50)), [])
 
     return (
-        <Navbar id="navbar" className={scroll ? 'navbar-shrink' : ''}>
+        <Navbar id="navbar" className={scroll ? 'navbar-shrink' : ''} expand="xl">
             <Link to="/">
-                <NavbarBrand>
+                <Navbar.Brand>
                     <img src={scroll ? logoColor : logoLight} alt="Logo"/>
                     <h6 className={(scroll ? 'text-black' : 'text-white') + ' font-w700 m-0'}>Houston ARTCC</h6>
-                </NavbarBrand>
+                </Navbar.Brand>
             </Link>
-            <Nav>
-                <Nav.Link className={scroll ? 'text-black' : ''}>Calendar</Nav.Link>
-                <Nav.Link className={scroll ? 'text-black' : ''}>Events</Nav.Link>
-                <Nav.Link className={scroll ? 'text-black' : ''}>Pilots</Nav.Link>
-                <Nav.Link className={scroll ? 'text-black' : ''}>Controllers</Nav.Link>
-                {parseJWT()
-                    ? <Nav.Link className={scroll ? 'text-black' : ''}>{getFullName()}</Nav.Link>
-                    : <Nav.Item className="ml-4">
-                        <Link to="/login">
+            <Navbar.Collapse>
+                <Nav>
+                    <Nav.Link as={Link} to="" className={scroll ? 'text-black' : 'text-white'}>Calendar</Nav.Link>
+                    <Nav.Link as={Link} to="/events" className={scroll ? 'text-black' : 'text-white'}>Events</Nav.Link>
+                    <NavDropdown className={scroll ? 'text-black' : 'text-white'} title="Pilots" id="nav-dropdown-pilots">
+                        <NavDropdown.Item as={Link} to="">Leave Feedback</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="">ARTCC Map</NavDropdown.Item>
+                        <NavDropdown.Item href="https://simcharts.info/">Charts</NavDropdown.Item>
+                        <NavDropdown.Item href="https://flightaware.com/statistics/ifr-route/">Routes</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown className={scroll ? 'text-black' : 'text-white'} title="Controllers" id="nav-dropdown-controllers">
+                        <NavDropdown.Item as={Link} to="/roster">Roster</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="">Staff</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="">IDS</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="">Resources</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to="">Statistics</NavDropdown.Item>
+                    </NavDropdown>
+                    {parseJWT()
+                        ?<NavDropdown className={scroll ? 'text-black' : 'text-white'} title={getFullName()} id="nav-dropdown-user">
+                            <NavDropdown.Item as={Link} to="">My Profile</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="">Training Center</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="">Administration</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item as={Link} to="/logout">Log Out</NavDropdown.Item>
+                        </NavDropdown>
+                        : <Nav.Item as={Link} to="/login" className="ml-4">
                             <Button variant="vatsim">
                                 <span className="font-w700">Login with VATSIM</span>
                             </Button>
-                        </Link>
-                    </Nav.Item>
-                }
-            </Nav>
+                        </Nav.Item>
+                    }
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
