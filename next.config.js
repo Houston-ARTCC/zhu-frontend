c887c3707b452f3f6eefd757dbc3df6c8766d425
Michael Romashov <mromashov@icloud.com>
2024-03-16 03:58:02 -0400

    output standalone module
    adjust tar directory

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     images: {
         domains: ['localhost', 'api.zhuartcc.org', 'api.zhuartcc.devel'],
     },
