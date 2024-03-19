4c25eae6bee1417eb02af21fbd1b4707f280081a
Michael Romashov <mromashov@icloud.com>
2024-03-18 23:52:24 -0400

    update dependencies

--- src/components/static/Toastify.tsx
+++ src/components/static/Toastify.tsx
@@ -5,6 +5,7 @@ import { ToastContainer } from 'react-toastify';
 
 export const Toastify: React.FC = () => (
     <ToastContainer
+        stacked
         position="bottom-right"
         autoClose={3000}
         hideProgressBar
