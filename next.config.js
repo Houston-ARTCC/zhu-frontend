d731255ae8cd404d1ead5327afd546fdd8134ad3
Michael Romashov <mromashov@icloud.com>
2024-03-25 23:05:57 -0400

    fix images not loading in prod

--- next.config.js
+++ next.config.js
@@ -6,16 +6,8 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '8000',
-                pathname: '/media/**',
-            },
-            {
                 protocol: 'https',
                 hostname: 'api.houston.center',
-                port: '80',
-                pathname: '/media/**',
             },
         ],
     },
