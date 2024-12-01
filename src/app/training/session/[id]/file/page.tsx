7ad6efd027acd4d673ef9a3a8f2c29757ad0bde6
William McKinnerney <git@williammck.net>
2024-12-01 01:47:01 -0600

    Missed an alert

--- src/app/training/session/[id]/file/page.tsx
+++ src/app/training/session/[id]/file/page.tsx
@@ -3,6 +3,7 @@ import { type NextPage } from 'next';
 import { LuAlertCircle } from 'react-icons/lu';
 import { Page } from '@/components/Page';
 import { PageContent } from '@/components/PageContent';
+import { Alert, AlertTitle } from '@/components/Alert';
 import { fetchApi } from '@/utils/fetch';
 import { type TrainingSession } from '@/types/training';
 import { type Staff } from '@/types/users';
@@ -49,28 +50,21 @@ const FileSession: NextPage<FileSessionParams> = async ({ params }) => {
     return (
         <Page {...metadata}>
             <PageContent>
-                <div className="mb-16 rounded-md bg-amber-500/10 py-5 pl-7 pr-10 text-amber-500">
-                    <div className="flex gap-3">
-                        <div className="pt-1">
-                            <LuAlertCircle size={25} />
-                        </div>
-                        <div>
-                            <h4 className="mb-0.5 text-2xl font-medium">Read before submitting!</h4>
-                            <p className="mb-3">
-                                Submitting this form will automatically submit this training session to the VATUSA Centralized Training Record System,
-                                thus there is no need to create this session on the VATUSA website! That being said, do not submit this form if the
-                                student failed to appear for the session or if the session was cancelled.
-                            </p>
-                            <p>
-                                If this session is an OTS examination, submit the OTS form separately as a supplement to this note at
-                                {' '}
-                                <a href="https://www.vatusa.net/mgt/controller/1339822/promote" target="_blank" rel="noreferrer">
-                                    https://www.vatusa.net/mgt/controller/1339822/promote
-                                </a>.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                <Alert color="amber-500" icon={LuAlertCircle} className="mb-16">
+                    <AlertTitle>Read before submitting!</AlertTitle>
+                    <p className="mb-3">
+                        Submitting this form will automatically submit this training session to the VATUSA Centralized Training Record System,
+                        thus there is no need to create this session on the VATUSA website! That being said, do not submit this form if the
+                        student failed to appear for the session or if the session was cancelled.
+                    </p>
+                    <p>
+                        If this session is an OTS examination, submit the OTS form separately as a supplement to this note at
+                        {' '}
+                        <a href="https://www.vatusa.net/mgt/controller/1339822/promote" target="_blank" rel="noreferrer">
+                            https://www.vatusa.net/mgt/controller/1339822/promote
+                        </a>.
+                    </p>
+                </Alert>
                 <SessionForm
                     session={trainingSession}
                     instructorOptions={options}
